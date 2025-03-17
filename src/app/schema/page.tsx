2a1936f440fe72f44e4e3c75207eee724c51738c
sirisayshello <veckovis.salongen0p@icloud.com>
2025-03-17 19:11:25 +0100

    schema design update wiop

--- src/app/schema/page.tsx
+++ src/app/schema/page.tsx
@@ -1,5 +1,13 @@
 import { SparklingWine } from "../components/SparklingWine";
 
+const ListItem = ({ time, info }: { time: string; info: string }) => {
+  return (
+    <li className="grid grid-cols-2">
+      <span className="">{time}</span> - {info}
+    </li>
+  );
+};
+
 export default function WishList() {
   return (
     <div className="flex flex-col justify-between h-full">
@@ -7,16 +15,19 @@ export default function WishList() {
         <h1>Dagens Schema</h1>
         <img src="/line.svg" alt="divider" className="w-24 mb-4" />
         <ul className="text-4xl flex flex-col mt-6 gap-4">
-          <li>14:30 - Trädgårdsmingel på Gathenheimska Huset</li>
-          <li>15:00 - Vigsel</li>
-          <li>- Bubbel och snacks och mer mingel -</li>
-          <li>
-            ca 16:30 - Dags att bege sig mot Taverna Efessos - en promenad på ca
-            800m eller två hållplatser med spårvagnen.
-          </li>
-          <li>17:00 - Middag på Efessos</li>
-          <li>00:00 - Efterfest på Efessos</li>
-          <li>01:00 - Efessos stänger</li>
+          <ListItem
+            time="14:30"
+            info="Trädgårdsmingel på Gathenheimska Huset"
+          />
+          <ListItem time="15:00" info="Vigsel" />
+          <ListItem time="" info="Bubbel och snacks och mer mingel" />
+          <ListItem
+            time="ca 16:30"
+            info="Dags att bege sig mot Taverna Efessos - en promenad på ca 800m eller två hållplatser med spårvagnen."
+          />
+          <ListItem time="17:00" info="Middag på Taverna Efessos" />
+          <ListItem time="00:00" info="Efterfest på Efessos" />
+          <ListItem time="01:00" info="Efesson stänger" />
         </ul>
       </div>
       <div>
