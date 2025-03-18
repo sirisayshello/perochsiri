b51023ea705e6ba7bec12f8db6bd2672918c8c42
sirisayshello <veckovis.salongen0p@icloud.com>
2025-03-18 22:58:32 +0100

    refactor Gift and GiftList

--- src/app/onskelista/GiftList.tsx
+++ src/app/onskelista/GiftList.tsx
@@ -1,19 +1,21 @@
-'use client'
+"use client";
 
 import { Spinner } from "../components/Spinner";
 import { Gift } from "./Gift";
 import { useGifts } from "./useGifts";
 
 export const GiftList = () => {
-    const { gifts, status } = useGifts();
+  const { gifts, status } = useGifts();
 
-    if (status === "loading") {
-        return <Spinner />;
-    }
+  if (status === "loading") {
+    return <Spinner />;
+  }
 
-    return <ul className="text-4xl flex flex-col mt-6 gap-4">
-        {gifts.map((gift) => {
-            return <Gift key={gift.name} {...gift} />;
-        })}
+  return (
+    <ul className="text-3xl flex flex-col w-full items-start mt-6 gap-4">
+      {gifts.map((gift) => {
+        return <Gift key={gift.name} {...gift} />;
+      })}
     </ul>
-}
+  );
+};
--- src/app/onskelista/page.tsx
+++ src/app/onskelista/page.tsx
@@ -6,16 +6,14 @@ import { GiftProvider } from "./useGifts";
 const WISHLIST_ENABLED = false;
 
 export default async function WishList() {
-
   return (
     <div className="flex flex-col justify-between h-full">
       <div className="flex flex-col items-center h-full">
         <h1>Önskelista</h1>
         <Line />
         {!WISHLIST_ENABLED && (
-          <p className="text-5xl mt-4">
-            Önskelistan kommer snart, håll ut!
-          </p>)}
+          <p className="text-5xl mt-4">Önskelistan kommer snart, håll ut!</p>
+        )}
         {WISHLIST_ENABLED && (
           <GiftProvider>
             <GiftList />
