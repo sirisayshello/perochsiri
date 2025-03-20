78873ecb673a7202f632978cd7ccc4d36d9a85e8
perbaltzar <per.baltzar@hiberworld.com>
2025-03-20 21:04:56 +0100

    show wishlist for admin

--- src/app/onskelista/page.tsx
+++ src/app/onskelista/page.tsx
@@ -1,20 +1,26 @@
+'use client'
+
 import { Line } from "../components/Line";
 import { WineGlass } from "../components/WineGlass";
+import { useIsAdmin } from "../hooks/useIsAdmin";
 import { GiftList } from "./GiftList";
 import { GiftProvider } from "./useGifts";
 
 const WISHLIST_ENABLED = false;
 
-export default async function WishList() {
+export default function WishList() {
+  const isAdmin = useIsAdmin();
+  const showWishlist = WISHLIST_ENABLED || isAdmin;
+
   return (
     <div className="flex flex-col justify-between h-full">
       <div className="flex flex-col items-center h-full">
         <h1>Önskelista</h1>
         <Line />
-        {!WISHLIST_ENABLED && (
+        {!showWishlist && (
           <p className="text-5xl mt-4">Önskelistan kommer snart, håll ut!</p>
         )}
-        {WISHLIST_ENABLED && (
+        {showWishlist && (
           <GiftProvider>
             <GiftList />
           </GiftProvider>
